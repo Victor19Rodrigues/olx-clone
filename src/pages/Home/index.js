cdf95a13b0e76ee6fe689ccfb3a1e702a442608d
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2020-02-04 06:53:46 -0300

    Created search area

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// import { Container } from './styles';
+import React, { useState } from "react";
+
+import useApi from "../../helpers/OlxAPI";
+
+import { PageContainer } from "../../components/MainComponents";
+import { PageArea, SearchArea } from "./styled";
 
 const Page = () => {
+  const api = useApi();
+
   return (
     <>
-      <div>Página Home</div>
-      <Link to="/about">Sobre</Link>
+      <SearchArea>
+        <PageContainer>
+          <PageArea>
+            <div className="searchBox">
+              <form action="/ads" method="GET">
+                <input
+                  type="text"
+                  name="q"
+                  id=""
+                  placeholder="O que você procura?"
+                />
+                <select name="state"></select>
+                <button>Pesquisar</button>
+              </form>
+            </div>
+            <div className="categorylist"></div>
+          </PageArea>
+        </PageContainer>
+      </SearchArea>
+
+      <PageContainer>
+        <PageArea>...</PageArea>
+      </PageContainer>
     </>
   );
 };
