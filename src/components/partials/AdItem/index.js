525612ddb74072e33a42fe442838f16d6d9afa62
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2020-02-04 08:27:47 -0300

    Created list of recent ads

--- src/components/partials/AdItem/index.js
+++ src/components/partials/AdItem/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Item } from "./styled";
 
 // import { Container } from './styles';
 
-export default () => {
-    return (
-        <div>...</div>
-    )
-}
+export default ({ data }) => {
+  let price = "";
+
+  if (data.priceNegotiable) {
+    price = "Preço Negociável";
+  } else {
+    price = `R$ ${data.price}`;
+  }
+
+  return (
+    <Item className="aditem">
+      <Link to={`/ad/${data.id}`}>
+        <div className="itemImage">
+          <img src={data.image} alt="" />
+        </div>
+        <div className="itemName">{data.title}</div>
+        <div className="itemPrice">{price}</div>
+      </Link>
+    </Item>
+  );
+};
---
+++ src/components/partials/AdItem/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Item = styled.div`
+  a {
+    display: block;
+    border: 1px solid #fff;
+    margin: 10px;
+    text-decoration: none;
+    color: #000;
+    padding: 10px;
+    border-radius: 5px;
+    background-color: #fff;
+    transition: all ease 0.2s;
+
+    &:hover {
+      border: 1px solid #ccc;
+      background-color: #eee;
+    }
+
+    .itemImage {
+      img {
+        width: 100%;
+        border-radius: 5px;
+      }
+    }
+
+    .itemName {
+      font-weight: bold;
+    }
+  }
+`;
