b3425700b54b2e4d542f07306a1f079a24b4e2fb
Victor Rodrigues <victor.rodrigues@justdigital.com.br>
2020-02-06 06:46:23 -0300

    Builded project

---
+++ build/precache-manifest.c3c4aa486598569f56a334183e2a9581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20fb1c2bff0e3a6596c0be4f19a919e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98745640bd98a17b1f49",
+    "url": "/static/css/2.b107a2aa.chunk.css"
+  },
+  {
+    "revision": "11e715026260e8fd46c7",
+    "url": "/static/css/main.8d414d52.chunk.css"
+  },
+  {
+    "revision": "98745640bd98a17b1f49",
+    "url": "/static/js/2.eac94e2d.chunk.js"
+  },
+  {
+    "revision": "4e47723aef56c4c7d3a45ec806795252",
+    "url": "/static/js/2.eac94e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e715026260e8fd46c7",
+    "url": "/static/js/main.55cd43bb.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
